c01605b2eafc4a7174f5efdb3285f3b6184a0ab9
Chimney <32008489+smallchimney@users.noreply.github.com>
2020-01-03 14:14:01 +0800

    New Feature: Add OpenMP support (#7)
    * Add OpenMP based speedup during the vocabulary building;
    
    * Renew the lisence

--- src/FeatureVector.cpp
+++ src/FeatureVector.cpp
@@ -1,5 +1,5 @@
 /**************************************************************************
- * Copyright (c) 2019 Chimney Xu. All Rights Reserve.
+ * Copyright (c) 2019-2020 Chimney Xu. All Rights Reserve.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
